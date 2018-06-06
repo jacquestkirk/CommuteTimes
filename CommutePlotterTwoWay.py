41f53d3d23220d0e3e32a8e6f4f3d0c204aef4d9
jacquestkirk <jacquestkirk@gmail.com>
2018-06-06 01:36:22 -0500

    Add new commute plotter that shows to work and to home on the same screen

---
+++ CommutePlotterTwoWay.py
@@ -0,0 +1,242 @@
+import pandas as pd
+import plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+import numpy as np
+
+apartmentsCsv = "apartmentLocations.txt"
+outputCsv = "output.csv"
+workCsv = "workLocations.txt"
+
+#colors for Wells Fargo and SpaceX respectively
+colors = ['red','blue']
+
+def readFile(fileName):
+
+
+    fileList = []
+
+    with open(fileName, "r") as file:
+        fileLines = file.readlines()
+        for line in fileLines:
+            #remove end line characters
+            lineToAppend = line.replace("\n", "")
+            lineToAppend = lineToAppend.replace("\r", "")
+            lineToAppend = lineToAppend.replace("&", "and") #replace this special character
+            lineToAppend = lineToAppend.replace(",", " ")
+
+            fileList.append(lineToAppend)
+
+    return fileList
+
+
+
+#read list of apartments
+apartmentLocations = readFile(apartmentsCsv)
+workLocations = readFile(workCsv)
+
+
+
+
+#read in the data
+df = pd.read_csv(outputCsv)
+df["Hour"] = pd.DatetimeIndex(df['Date time']).hour     #add in an hour column with the hour of the measurement
+
+
+
+
+#select locations
+apartmentIndex = 0
+chosenApartment = apartmentLocations[apartmentIndex]
+
+workIndex = 0
+chosenWork = workLocations[workIndex]
+
+#build up filters
+#originFilter = dict(
+#    type = 'filter',
+#    target = df['Origin'],
+#    operation = '=',
+#    value = chosenApartment
+#  )
+
+destinationFilter = dict(
+    type = 'filter',
+    target = df['Destination'],
+    operation = '=',
+    value = chosenWork
+  )
+
+destinationGroup = dict(
+        type = 'groupby',
+        groups = df['Destination'])
+
+#fig = py.tools.make_subplots(rows=len(apartmentLocations), subplot_titles=apartmentLocations, cols =1)
+
+data = []
+buttons = []
+#partmentLocations = apartmentLocations[0:4]
+for apartmentIndex in range(len(apartmentLocations)):
+    chosenApartment = apartmentLocations[apartmentIndex]
+
+    originFilter = dict(
+        type='filter',
+        target=df['Origin'],
+        operation='=',
+        value=chosenApartment
+    )
+
+    transformList = [originFilter, destinationGroup]
+
+    #apartment as origin
+    df_filtered_origin = df.query("Origin == " + '"'+chosenApartment+'"')
+
+    #apartment as destination
+    df_filtered_apart_destination = df.query("Destination == " + '"'+chosenApartment+'"')
+
+
+    for workIndex in range(len(workLocations)):
+        chosenWork = workLocations[workIndex]
+
+        #commute to wirk
+        df_filtered = df_filtered_origin.query("Destination == " + '"' + chosenWork + '"')
+
+        trace = dict(
+            type='scatter',
+            x=df_filtered["Hour"],
+            y=df_filtered["Time (min)"],
+            xaxis='x1',
+            yaxis='y1',
+            mode='markers',
+            name=chosenWork + "to work",
+            fillcolor= colors[workIndex]
+
+            #xaxis='xaxis' + str(apartmentIndex + 1),
+            #yaxis='yaxis' + str(apartmentIndex + 1),
+            #transforms=transformList
+        )
+
+        data.append(trace)
+
+        #commute home
+        df_filtered_work_origin = df_filtered_apart_destination.query("Origin == " + '"' + chosenWork + '"')
+        trace = dict(
+            type='scatter',
+            x=df_filtered_work_origin["Hour"],
+            y=df_filtered_work_origin["Time (min)"],
+            xaxis='x2',
+            yaxis='y2',
+            mode='markers',
+            name=chosenWork + "to home",
+            fillcolor=colors[workIndex],
+            shapes=[
+                # filled Rectangle
+                {
+                    'type': 'rect',
+                    'x0': 6,
+                    'y0': 0,
+                    'x1': 10,
+                    'y1': 30,
+                    'line': {
+                        'color': 'rgba(128, 0, 128, 1)',
+                        'width': 2,
+                    },
+                    'fillcolor': 'rgba(128, 0, 128, 0.7)',
+                },
+            ]
+
+            # xaxis='xaxis' + str(apartmentIndex + 1),
+            # yaxis='yaxis' + str(apartmentIndex + 1),
+            # transforms=transformList
+        )
+
+        data.append(trace)
+
+    plotsPerCombo = 4
+    visibleList = [False]*plotsPerCombo*len(apartmentLocations)
+    visibleList[plotsPerCombo*apartmentIndex:plotsPerCombo*(apartmentIndex+1)] = [True]*plotsPerCombo
+
+    button = dict(label=chosenApartment,
+                  method='update',
+                  args=[{'visible': visibleList},
+                        {'title': chosenApartment}])
+
+    buttons.append(button)
+
+updatemenus = list([
+    dict(active=-1,
+         buttons=buttons,
+    )
+])
+
+layout = {
+    "title": "CommuteTimes",
+    "updatemenus": updatemenus,
+    "xaxis1": {
+                'domain': [0.0, 0.45],
+                'anchor': 'y1',
+                },
+    "xaxis2": {
+            'domain': [0.55, 1.0],
+            'anchor': 'y2',
+            },
+    "yaxis1": {
+                'domain': [0.0, 1.0],
+                'anchor': 'x1',
+                },
+    "yaxis2": {
+            'domain': [0.0, 1.0],
+            'anchor': 'x2',
+             },
+    "shapes": [
+        # to work
+        {
+            'type': 'rect',
+            'x0': 6,
+            'y0': 0,
+            'x1': 10,
+            'y1': 30,
+            'xref':'x1',
+            'yref':'y1',
+            'line': {
+                'color': 'rgba(128, 0, 128, 1)',
+                'width': 2,
+            },
+            'fillcolor': 'rgba(128, 0, 128, 0.7)',
+        },
+        # back home
+        {
+            'type': 'rect',
+            'x0': 12+3,
+            'y0': 0,
+            'x1': 12+8,
+            'y1': 30,
+            'xref':'x2',
+            'yref':'y2',
+            'line': {
+                'color': 'rgba(128, 0, 128, 1)',
+                'width': 2,
+            },
+            'fillcolor': 'rgba(128, 0, 128, 0.7)',
+        },
+    ]
+}
+
+fig = {
+    "data": data,
+    "layout": layout
+}
+
+py.offline.plot(fig, filename='index.html', validate=False)
+
+
+
+
+
+##violin
+#df_filt = df
+#df_filt = df_filt.query("Origin == '" + chosenApartment +"'")
+#df_filt = df_filt.query("Destination == '" + chosenWork +"'")
+#fig = ff.create_violin(df_filt, data_header='Time (min)', group_header='Hour',
+#                       height=500, width=5000)
+#py.offline.plot(fig, filename='test.html', validate=False)
